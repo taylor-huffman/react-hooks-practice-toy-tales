61c36418c03b8df394d8eeadb456dae530e55d40
Taylor Huffman <taylor@crucialartists.com>
2022-03-16 13:59:36 -0500

    Complete assignment

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, handleDeleteToy, handleAddLike }) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{toys.map(toy => {
+      return <ToyCard key={toy.id} toy={toy} handleDeleteToy={handleDeleteToy} handleAddLike={handleAddLike} />
+    })}</div>
   );
 }
 
